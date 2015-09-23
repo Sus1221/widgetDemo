be0f10d6aab3a380ac45213f562f9d69b41f4560
Sus1221 <susanne881221@hotmail.com>
2015-09-23 10:15:12 +0200

    no jquery!

--- loader.js
+++ loader.js
@@ -1,6 +1,5 @@
-$(function() {
-    console.log("Loader.js working!");
-});
+console.log("Loader.js working!");
+
 
 //Att utgå ifrån -denna funkar bra som bookmarklet
 //javascript:(function(){document.getElementsByTagName('body')[0].style.setProperty('font-family', 'serif', 'important');})();
@@ -8,4 +7,4 @@ $(function() {
 
 //Code for inserting in bookmarklet adress
 //Denna gick in som adress - kolla bara sökvägen - kanske ändra till localhost medans jag utvecklar??
-//javascript:(function(){var%20script=document.createElement('script');script.type='text/javascript';script.src='https://github.com/Sus1221/widgetDemo/blob/master/loader.js';document.getElementsByTagName('head')[0].appendChild(script);})();
+//javascript:(function(){var%20script=document.createElement('script');script.type='text/javascript';script.src='https://rawgit.com/Sus1221/widgetDemo/master/loader.js';document.getElementsByTagName('head')[0].appendChild(script);})();
