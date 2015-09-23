3c4d2c8d04c922a50a92abe41ef6e405e96694d2
Sus1221 <susanne881221@hotmail.com>
2015-09-23 10:59:03 +0200

    Trying out jquery.

--- loader.js
+++ loader.js
@@ -1,23 +1,29 @@
-console.log("Loader.js working!");
-
-//Code for inserting in bookmarklet url
+//Code for inserting this script in bookmarklet url
 //javascript:(function(){var%20script=document.createElement('script');script.type='text/javascript';script.src='https://rawgit.com/Sus1221/widgetDemo/master/loader.js';document.getElementsByTagName('head')[0].appendChild(script);})();
 
+console.log("Loader.js working!");
+
+//Load javascript to the page
 function loadScript(url, callback) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
-    // Fire the loading
-    head.appendChild(script);
+	//"grab" the <head> in index.html
+	var head = document.getElementsByTagName('head')[0];
+	//Create a new <script>
+	var script = document.createElement('script');
+	//Assign values to script
+	script.type = 'text/javascript';
+	script.src = url;
+	//Bind callback function to events
+	script.onreadystatechange = callback;
+	script.onload = callback;
+	//Add <script> to <head>
+	head.appendChild(script);
 }
 
+//Callback after jquery load
 var callbackForLoadjQuery = function() {
-   console.log("callback for loading jquery!");
+  console.log("callback for loading jquery!");
 };
-
+//Run to load jquery script to page
 loadScript("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", callbackForLoadjQuery);
+
+$("body").prepend("<h1>Hellloo</h1>");
