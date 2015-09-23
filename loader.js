b506ae28c1baaea0b92585555de3ab053bd3f9be
Sus1221 <susanne881221@hotmail.com>
2015-09-23 10:35:31 +0200

    Testing to include jquery on page in js.

--- loader.js
+++ loader.js
@@ -1,10 +1,23 @@
 console.log("Loader.js working!");
 
-
-//Att utgå ifrån -denna funkar bra som bookmarklet
-//javascript:(function(){document.getElementsByTagName('body')[0].style.setProperty('font-family', 'serif', 'important');})();
-
-
-//Code for inserting in bookmarklet adress
-//Denna gick in som adress - kolla bara sökvägen - kanske ändra till localhost medans jag utvecklar??
+//Code for inserting in bookmarklet url
 //javascript:(function(){var%20script=document.createElement('script');script.type='text/javascript';script.src='https://rawgit.com/Sus1221/widgetDemo/master/loader.js';document.getElementsByTagName('head')[0].appendChild(script);})();
+
+function loadScript(url, callback) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+    // Fire the loading
+    head.appendChild(script);
+}
+
+var callbackForLoadjQuery = function() {
+   console.log("callback for loading jquery!");
+};
+
+loadScript("https://rawgit.com/Sus1221/widgetDemo/master/jquery-1.11.3.min.js", callbackForLoadjQuery);
