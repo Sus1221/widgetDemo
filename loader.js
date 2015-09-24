fd516f2eaaddf84efefc7b2946febe012513d5d5
Sus1221 <susanne881221@hotmail.com>
2015-09-24 14:50:45 +0200

    added resizable on the other widget-<div><<

--- loader.js
+++ loader.js
@@ -54,7 +54,7 @@ var callbackForLoadjQueryUI = function() {
 										);
 		//Make divs draggable with jQueryUI
 		$("#strossleWidgetLarge").draggable({scrollSpeed:500}).resizable({aspectRatio: true});
-		$("#strossleWidgetSmall").draggable({scrollSpeed:500});
+		$("#strossleWidgetSmall").draggable({scrollSpeed:500}).resizable({aspectRatio: true});
 };
 
 var callbackForLoadCSS = function() {
