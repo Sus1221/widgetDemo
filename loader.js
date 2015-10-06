bbfe9b3862366c2c66b016fb6fc5bf4674488767
Sus1221 <susanne881221@hotmail.com>
2015-10-06 11:06:08 +0200

    style and remove function to widget div created by user

--- loader.js
+++ loader.js
@@ -104,9 +104,13 @@ function calcDivMeasurements() {
 	}
 	console.log("divWidth: ", divWidth, "divHeight", divHeight);
 	console.log("clickedElement: ", clickedElement);
-	$("<div style='border:1px solid black; width:" + divWidth + "px;height:"+ divHeight +"px;z-index:200000000'></div>").insertBefore(clickedElement);
+	$("<div style='border:1px solid black; outline:1px solid darkgray; width:" + divWidth + "px;height:"+ divHeight +"px;z-index:200000000'><h3>style='position:absolute;top:2px;right:2px' class:'XtoRemoveStrossleWidgetDiv'>X</h3></div>").insertBefore(clickedElement);
 	//makeBodyContentSortable();
 }
+
+$(".XtoRemoveStrossleWidgetDiv").click(function() {
+	console.log("Clicked on X to remove parent div");
+});
 
 //Makes <body> and some of its descendants sortable
 function makeBodyContentSortable() {
