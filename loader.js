e1ed1506c9e1a74a267a01077d5a167b3fd444f3
Sus1221 <susanne881221@hotmail.com>
2015-10-08 14:18:30 +0200

    working on check if clickedElement is <body>

--- loader.js
+++ loader.js
@@ -117,7 +117,7 @@ function calcDivMeasurements() {
 	console.log("clickedelement type", clickedElement.type);
 	if(divHeight > 50 && divWidth > 50) {
 		var divToAdd = "<div style='position:relative; border:1px solid black; outline:1px solid darkgray; background:white; width:" + divWidth + "px;height:"+ divHeight + "px;margin:5px;z-index:200000000'><h3 class='XtoRemoveStrossleWidgetDiv' style='position:absolute;top:5px;right:5px;cursor:pointer;font-size:30px;color:black;'>&#10006;</h3></div>";
-		if(clickedElement.body){
+		if(clickedElement == $("body")){
 			console.log("The clicked div is body!");
 			//prepend div to body
 			$("body").prepend(divToAdd);
