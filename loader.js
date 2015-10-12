7c652c4797bcb782b6e11ed888882378823eb4b6
Sus1221 <susanne881221@hotmail.com>
2015-10-12 15:56:41 +0200

    time adjustment for settimeout

--- loader.js
+++ loader.js
@@ -98,9 +98,9 @@ function whichElementClicked(event){
 	//console.log("clickedElement.type", clickedElement.type);
 	if(clickedElement.type == "radio"){
 		console.log("it it a radiobutton");
-		setTimeout(function() { manageUserFunctions(event); }, 5000);
+		setTimeout(function() { manageUserFunctions(event); }, 1000);
 	}
-	
+
 	//If X (remove) on widget is clicked
 	if(clickedElement.className.indexOf("XtoRemoveStrossleWidgetDiv") > -1){
 		console.log("It's the right class name, it is X");
