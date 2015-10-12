965e2bf94227ee195f3bae0d24924bc8a34d69c9
Sus1221 <susanne881221@hotmail.com>
2015-10-12 10:30:37 +0200

    radiobuttons syntax error fix

--- loader.js
+++ loader.js
@@ -44,10 +44,10 @@ var callbackForLoadjQueryUI = function() {
 	//Add controlBox for sortable()/selectable()
 	$("body").prepend("<div style='min-width:200px;min-height:100px;background-color:white;border:1px solid black;padding:5px;z-index: 200000;position:fixed;top:0' id='choiceBox'>" +
 												"<form>" +
-													"<label for='createElements'>Create Elements</label>" +
-													"<input type='radio' name='createElements' id='createElements'><br>" +
-													"<label for='sortElements'>Drag n drop</label>" +
-													"<input type='radio' name='sortElements' id='sortElements'><br>" +
+														"<label for='createElements'>Create Elements</label>" +
+														"<input type='radio' name='chooseFunction' id='createElements'><br>" +
+														"<label for='sortElements'>Drag n drop</label>" +
+														"<input type='radio' name='chooseFunction' id='sortElements'><br>" +
 												"</form>" +
 											"</div>");
 	$("body").selectable({
