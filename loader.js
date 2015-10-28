c5b784f5edee0034cc61d96e9e69c3705dec6264
Sus1221 <susanne881221@hotmail.com>
2015-10-28 19:02:06 +0100

    added a second widget to load on page load

--- loader.js
+++ loader.js
@@ -87,12 +87,15 @@ function callbackForLoadjQueryUI() {
 }
 
 function testToAddWidgetOnLoad(){
-	console.log("test to add widget on load function");
-	var divToAddOnLoad = "<div style='display: inline-block; position:relative; float:left; border:"+ borderStyle +"; background:white;width:1076px;height:300px;margin:20px;z-index:200000000;overflow:hidden' class='widgetDiv'><h4 class='XtoRemoveStrossleWidgetDiv' style='position:absolute;top:1px;right:5px;cursor:pointer;font-size:15px;color:black;z-index:2000000000'>&#10006;</h4>" +
+	//standard widget
+	var standardWidgetDiv = "<div style='display: inline-block; position:relative; margin: 0 auto; border:"+ borderStyle +"; background:white;width:1076px;height:310px;margin:20px;z-index:200000000;overflow:hidden' class='widgetDiv'><h4 class='XtoRemoveStrossleWidgetDiv' style='position:absolute;top:1px;right:5px;cursor:pointer;font-size:15px;color:black;z-index:2000000000'>&#10006;</h4>" +
 								strossleWidgetDiv + "</div>";
-	var elementToAppendTo = $("body > *:last-of-type:not(script):not(noscript)").last();
-	console.log("elementToAppendTo", elementToAppendTo);
-	$(divToAddOnLoad).insertAfter(elementToAppendTo);
+	var elementToInsertAfter = $("body > *:last-of-type:not(script):not(noscript)").last();
+	$(standardWidgetDiv).insertAfter(elementToInsertAfter);
+	//sidebar widget
+	var sidebarWidgetDiv = "<div style='display: inline-block; position:relative; float:right; border:"+ borderStyle +"; background:white;width:100px;height:500px;margin:20px;z-index:200000000;overflow:hidden' class='widgetDiv'><h4 class='XtoRemoveStrossleWidgetDiv' style='position:absolute;top:1px;right:5px;cursor:pointer;font-size:15px;color:black;z-index:2000000000'>&#10006;</h4><div data-spklw-widget='widget-5524d25c249ad'></div></div>";
+	var elementToPrependTo = $("body > *").first();
+	$(elementToPrependTo).prepend(sidebarWidgetDiv);
 	makeWidgetResizable();
 	makeWidgetDraggable();
 	//'tr:not(.table_vert_controls):last'
