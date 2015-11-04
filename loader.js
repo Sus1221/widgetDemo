e4293ecd69ab642bf9fd287b22f135c3f01731c1
Sus1221 <susanne881221@hotmail.com>
2015-11-04 14:37:03 +0100

    commenting and cleaning of code

--- loader.js
+++ loader.js
@@ -1,41 +1,42 @@
 //Insert code on row below in bookmark url-field in browser to run this script on current url
 //javascript:(function(){var%20script=document.createElement('script');script.type='text/javascript';script.src='https://rawgit.com/Sus1221/widgetDemo/master/loader.js';document.getElementsByTagName('head')[0].appendChild(script);})();
 
-//Load files to current page
+//Load files to page
 function loadFile(type, url, callback, async) {
-	//"grab" the <head> in index.html
+	//"grab" the <head> in index.html of page
 	var head = document.getElementsByTagName('head')[0];
+	//If function call sends in type "js"
 	if(type == "js"){
 		//Create a new <script>
 		var script = document.createElement('script');
-		//Assign values to script
+		//Assign attributes to <script>
 		script.type = 'text/javascript';
 		script.src = url;
+		//If the script should load asynchronously
 		if(async){
-			//console.log("async added!");
 			script.async = true;
 		}
-		//if sprinkle content script is loaded
+		//If sprinkle content script is being loaded
 		if (url.indexOf("sprinklecontent") >= 0){
-			//add attribute
-			//console.log("special att added");
+			//add attribute needed
 			var att = document.createAttribute("data-spklw-automatic-initialization");
 			script.setAttributeNode(att);
 		}
-		//Bind ev. callback function to events
+		//Bind eventual callback function to events
 		if(callback){
 			script.onreadystatechange = callback;
 			script.onload = callback;
 		}
 		//Add <script> to <head>
 		head.appendChild(script);
+	//If function call sends in type "css"
 	}else if(type == "css"){
 		//Create a new <link>
 		var link = document.createElement('link');
-		//Assign values to link
+		//Assign attributes to link
 		link.rel = 'stylesheet';
 		link.href = url;
-		//Bind callback function to events
+		//Bind eventual callback function to events
 		link.onreadystatechange = callback;
 		link.onload = callback;
 		//Add <link> to <head>
@@ -53,13 +54,12 @@ var divWidth = 0;
 var divHeight = 0;
 var clickedElement;
 var divToAdd = "";
-//The default/start div is a standard widget
+//The default/start div, a standard widget
 var strossleWidgetDiv = "<div data-spklw-widget='widget-5591293a1ed53'></div>";
 var borderStyle = "1px solid black";
 
 //Callback after jQuery load
 function callbackForLoadjQuery() {
-	console.log("jquery loaded");
 	//load jQueryUI script to site
 	loadFile("js", "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js", callbackForLoadjQueryUI);
 	//Load jquery ui's css to site
@@ -68,8 +68,7 @@ function callbackForLoadjQuery() {
 
 //Callback after jqueryUI load
 function callbackForLoadjQueryUI() {
-	console.log("callback for load jquery ui function");
-	//Add controlBox for widget-<div>'s
+	//Add a 'controlbox' to page to control widget-<div>'s
 	$("body").prepend("<div id ='controlBox' style='text-align:left;min-width:30px;min-height:30px;background-color:white;border:1px solid black;padding:5px;z-index: 200000001;position:fixed;top:0;left:0'>" +
 										"<img src='https://pbs.twimg.com/profile_images/641610044036018176/OQzkinPw.png' style='height:40px;width:auto;margin:3px;display:block;'><br>" +
 										"<input type='checkbox' id='border' name='border' checked>" +
@@ -83,43 +82,29 @@ function callbackForLoadjQueryUI() {
 	makeBodySelectable();
 }
 
-//onmousedown for body
+//onmousedown click for <body>
 function whichElementClicked(event){
-	//clickedElement = clicked html element
+	//clickedElement = the clicked html element
 	clickedElement = event.target;
-	console.log("clickedElement", clickedElement);
 	//If checkbox for border is clicked
 	if(clickedElement.id == "border"){
-		console.log("clicked element:", clickedElement);
 		setTimeout(function(){manageDivBorder();}, 200);
 	}
-	//if radio buttons for widgetType are clicked
+	//if radio buttons for widgetType choice are clicked
 	if(clickedElement.className.indexOf("widgetType") > -1){
-		console.log("widgettype input class clicked");
 		setTimeout(function(){manageWidgetType();}, 200);
 	}
-	//If X (remove) on widget is clicked
+	//If X (remove) in a widget is clicked
 	if(clickedElement.className.indexOf("XtoRemoveStrossleWidgetDiv") > -1 ){
 		console.log("you clicked XtoRemoveStrossleWidgetDiv");
-		//remove that widget div
+		//remove that widget-<div>
 		$(clickedElement).parent().parent().remove();
-		console.log("Element removed");
-	}
-	//De tre if-satserna har att göra med felsökning för blt.se
-	if(clickedElement.className.indexOf("emoji") > -1){
-		console.log("element has class emoju");
-	}
-	if($(clickedElement).hasClass("emoji")){
-		console.log("hasClass emoji");
-	}
-	if(clickedElement.type == "img"){
-		console.log("element is type img");
 	}
 }
 
 //manage borderstyle of .outerWidgetDiv
 function manageDivBorder() {
-	console.log("manageDivBorder Function");
+	//if checkbox is checked
 	if($("#border").is(":checked")){
 			borderStyle = "1px solid black";
 		}else{
@@ -130,13 +115,13 @@ function manageDivBorder() {
 
 //switches widget content source
 function manageWidgetType() {
+	//If standard widget is chosen
 	if($("#standardWidget").is(":checked")){
-		console.log("standard widget is checked");
 		//use Strossle's standard widget
 		strossleWidgetDiv = "<div data-spklw-widget='widget-5591293a1ed53'></div>";
 	}
+	//If sidebar widget is chosen
 	if($("#sidebarWidget").is(":checked")){
-		console.log("sidebar widget is checked");
 		//use Strossle's sidebar widget
 		strossleWidgetDiv = "<div data-spklw-widget='widget-5524d25c249ad'></div>";
 	}
@@ -144,19 +129,14 @@ function manageWidgetType() {
 
 //Makes <body> "selectable" - makes it possible for user to create custom <div>
 function makeBodySelectable() {
-	console.log("body now selectable");
 	$("html").selectable({
 		start: function(event, ui){
-			console.log("event", event);
 			startX = event.pageX;
 			startY = event.pageY;
-			console.log("x, y:", startX, startY);
 		},
 		stop: function(event, ui){
-			console.log("event", event);
 			endX = event.pageX;
 			endY = event.pageY;
-			console.log("x, y:", endX, endY);
 			calcDivMeasurements();
 		}
 	});
@@ -167,45 +147,42 @@ function calcDivMeasurements() {
 	var leftToRight;
 	var topToBottom;
 	var positionRules = "";
-	console.log("calcDivMeasurements start. startx, endx, starty, endy", startX, endX, startY, endY);
-	//Calculate divWidth
+	//If horizontal number is higher than on start
 	if(endX > startX){
 		leftToRight = true;
 		divWidth = endX - startX;
+	//If horizontal number is lower than on start
 	}else {
 		leftToRight = false;
 		divWidth = startX - endX;
 	}
-	//Calculate divHeight
+	//If vertical number is higher than on start
 	if(endY > startY){
 		topToBottom = true;
 		divHeight = endY - startY;
+	//If vertical number is lower than on start
 	}else{
 		topToBottom = false;
 		divHeight = startY - endY;
 	}
 	//if user "draws" widgetbox from left to right
 	if(leftToRight){
-		console.log("left to right!");
 		positionRules += "left:" + startX + "px;";
 	//if user "draws" widgetBox from right to left
 	}else{
-		console.log("right to left!");
 		positionRules += "left:" + endX + "px;";
 	}
 	//if user "draws" widgetbox from top to bottom
 	if(topToBottom){
-		console.log("top to bottom!");
 		positionRules += "top:" + startY + "px;";
 	//uf user "draws" widgetbox from bottom to top
 	}else{
-		console.log("bottom to top");
 		positionRules += "top:" + endY + "px;";
 	}
-
+	//If userdrawn box have sufficient measurements
 	if(divHeight > 200 && divWidth > 100) {
 		console.log("position rules", positionRules);
-		//For remove functionality to work, the X sign (&#10006) must be a grandchild of .XtoRemoveStrossleWidgetDiv
+		//For the remove-functionality to work, the X sign must be a grandchild of .XtoRemoveStrossleWidgetDiv
 		divToAdd = "<div style='position:absolute;"+ positionRules + "z-index:200000000;background:white;overflow:hidden;display:inline-block;" +
 						"border:" + borderStyle + ";width:" + divWidth + "px;height:" + divHeight + "px;' class='outerWidgetDiv'>" +
 							"<div style='display: inline-block; position:relative;width:100%;background:white;' class='widgetDiv'>" +
@@ -222,6 +199,7 @@ function calcDivMeasurements() {
 	}
 }
 
+//makes widget resizable for user
 function makeWidgetResizable() {
    console.log("widget now resizable");
    $(".outerWidgetDiv").resizable({
@@ -232,14 +210,13 @@ function makeWidgetResizable() {
    });
 }
 
+//makes widge drag'n'droppable for user
 function makeWidgetDraggable() {
 	console.log("make widget draggable function");
 	$(".outerWidgetDiv").draggable();
 	$("#controlBox").draggable();
 }
 
-//EXECUTION 
-/*****************************************************************/
 //load jQuery to site
 loadFile("js", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", callbackForLoadjQuery);
 loadFile("js", "http://widgets.sprinklecontent.com/v2/sprinkle.js", false, true);
