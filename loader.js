c3c264f95616c4b7b7dac6b5f3430b80f070923f
Sus1221 <susanne881221@hotmail.com>
2015-11-05 15:52:37 +0100

    added .show() to get errorsmg to show second time

--- loader.js
+++ loader.js
@@ -236,9 +236,8 @@ function makeWidgetDraggable() {
 
 function showTooShortMessage(){
 	console.log("The minimum measurements for the box are: ",minHeight, "px high and ", minWidth, " px wide.");
-	$("#lengthErrorMessage").html("<hr><p>Minimum measurements <br>for chosen widget<br>type are: <br>height: " + minHeight + "px,<br>width: " + minWidth + " px.</p>");
+	$("#lengthErrorMessage").html("<hr><p>Minimum measurements <br>for chosen widget<br>type are: <br>height: " + minHeight + "px,<br>width: " + minWidth + " px.</p>").show();
 	setTimeout(function(){$("#lengthErrorMessage").fadeOut(1000);}, 10000);
-	$("#lengthErrorMessage").html();
 }
 
 
