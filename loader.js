070a7b8b4827f37aee9703d589fa17fb29bf70ea
Sus1221 <susanne881221@hotmail.com>
2015-11-11 10:53:03 +0100

    moved 123 to own container to get positioning right

--- loader.js
+++ loader.js
@@ -242,6 +242,8 @@ function calcDivMeasurements() {
 						"border:" + borderStyle + ";width:" + divWidth + "px;height:" + divHeight + "px;' class='outerWidgetDiv'>" +
 							"<div style='display:inline-block; position:relative;width:100%;background:white;' class='widgetDiv'>" +
 								strossleWidgetDiv +
+							"</div>" +
+							"<div style='position:absolute;bottom:1px;left:1px'>" +
 								"<h5 class='widget1' style='cursor:pointer;display:inline;margin:7px;font-weight:bold;'>1</h5>" +
 								"<h5 class='widget2' style='cursor:pointer;display:inline;margin:7px;font-weight:normal;'>2</h5>" +
 								"<h5 class='widget3' style='cursor:pointer;display:inline;margin:7px;font-weight:normal;'>3</h5>" +
